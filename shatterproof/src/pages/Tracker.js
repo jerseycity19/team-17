b068737387cc8523a0d098450e82eed3e1c2382c
sophiagiordano <sgiordano99@gmail.com>
2019-09-28 11:46:38 -0400

    edit Tracker page

--- shatterproof/src/pages/Tracker.js
+++ shatterproof/src/pages/Tracker.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Calendar from './Calendar.js';
 import Container from '@material-ui/core/Container';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Grid from '@material-ui/core/Grid';
+import { teal } from '@material-ui/core/colors';
+
+const ColoredLine = () => (
+  <hr
+      style = {{
+          color: teal[300],
+          backgroundColor: teal[300],
+          height: 5
+      }}
+  />
+)
 
 class Tracker extends React.Component {
     constructor(props) {
@@ -19,7 +32,8 @@ class Tracker extends React.Component {
         const { startDate, endDate } = this.state
     
         return (
-            <Container>
+          <div>
+            {/* <Container>
               <div className = "calendarWrapper"> 
               <div className="CalTitle">
                 <p> <strong>TRACK YOUR VICTORIES </strong></p>
@@ -31,6 +45,62 @@ class Tracker extends React.Component {
               <Calendar startDate={startDate} endDate={endDate} onChange={this.onChange} range displayTime />
               </div>
             </Container>
+            <Container>
+              <ProgressBar striped variant="info" now={20} />
+            </Container> */}
+            <Grid container spacing={1}>
+                <Grid item xs={12}>
+                  <div className = "calendarWrapper"> 
+                    <div className="CalTitle">
+                      <p> <strong>TRACK YOUR VICTORIES </strong></p>
+                    </div>
+                    <div className="TrackerInfo">
+                      <p> Use this calendar to track your days without use! </p>
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <div align="center">
+                      <br/>
+                      <br/>
+                      <br/>
+                      <ColoredLine />
+                  </div>
+                  <div className = "calendarWrapper"> 
+                    <p> I HAVE BEEN WITHOUT SUBSTANCE USE FOR </p>
+                    <p> <strong> 5 DAYS </strong></p>
+                  </div>
+                  <div align="center">
+                      <ColoredLine />
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <div className = "calendarWrapper"> 
+                    <Calendar startDate={startDate} endDate={endDate} onChange={this.onChange} range displayTime />
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                <div align="center">
+                      <br/>
+                      <br/>
+                      <br/>
+                      <ColoredLine />
+                  </div>
+                <div className = "calendarWrapper"> 
+                    <p> I AM </p>
+                    <p> <strong> #STRONGER THAN ADDICTION </strong></p>
+                  </div>
+                  <div align="center">
+                      <ColoredLine />
+                  </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <br/>
+                    <ProgressBar striped variant="warning" now={40} />
+                    <br/>
+                </Grid>
+            </Grid>
+          </div>
         )
       }
 }
