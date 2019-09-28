77962671fe0c1edc16535c1651092ae306d49601
Ally Gilbert <allygilbert2017@yahoo.com>
2019-09-28 06:25:48 -0400

    merge conflicts resolved

--- shatterproof/src/components/TabButtons.js
+++ shatterproof/src/components/TabButtons.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Resources from '../pages/Resources.js';
 
 const styles = {
     container:{
@@ -25,24 +22,11 @@ const styles = {
         display: 'none',
   },
 };
-  input: {
-    display: 'none',
-  },
-}));
-
-const OutlinedButtons =({
-  history
-})=> {
-  const classes = useStyles();
 
 const  OutlinedButtons=({classes})=> {
   return (
         <div  className={classes.container}>
             <Button className={classes.button}>
-    <div>
-      <Container>
-        <Typography component="div" style={{ backgroundColor: '#6DcBBD', height: '10vh' }}>
-            <Button className={classes.button} onClick={()=>history.push('/resources')}>
                 Resources
             </Button>
             <Button className={classes.button}>
@@ -54,10 +38,10 @@ const  OutlinedButtons=({classes})=> {
             <Button className={classes.button}>
                 Tracker
             </Button>
-            <Button className={classes.button} onClick={()=>history.push('/donate')}>
-                <a href="https://connect.clickandpledge.com/w/Form/4e21c330-0011-430e-a2c0-5400c4ba45d5" target="_blank">Donate</a>
+            <Button className={classes.button}>
+              <a href="https://connect.clickandpledge.com/w/Form/4e21c330-0011-430e-a2c0-5400c4ba45d5" target="_blank">Donate</a>
             </Button>
-            <Button className={classes.button} onClick={()=>history.push('/fundraise')}>
+            <Button className={classes.button}>
                 <a href="https://www.shatterproof.org/get-involved" target="_blank">Fundraise</a>
             </Button>
         </div>
@@ -65,11 +49,3 @@ const  OutlinedButtons=({classes})=> {
 }
 
 export default withStyles(styles)(OutlinedButtons);
-        </Typography> 
-        
-      </Container>
-      </div>
-    );
-}
-
-export default OutlinedButtons;
--- shatterproof/src/pages/Home.js
+++ shatterproof/src/pages/Home.js
@@ -16,23 +16,13 @@ class Home extends React.Component {
                     addiction treatment, shattering stigma of addiction, advocating
                     for federal and state policy change and prayer reform, and supporting
                     and educating the community. </p>
-<<<<<<< HEAD
-=======
 
-                    <div className="Footer-Logos">
-                        <div className="Logos">
-                            <SocialIcon url="https://twitter.com/shatterproofhq?lang=en" target="_blank" style={{margin: 8}}/>
-                            <SocialIcon url="https://www.facebook.com/ShatterproofHQ/?ref=nf" target="_blank" style={{margin: 8}}/>
-                            <SocialIcon url="https://www.instagram.com/weareshatterproof/?hl=en" target="_blank" style={{margin: 8}}/>
-                        </div>
-                    </div>
->>>>>>> ally
                 </div>
                 <footer className="Footer-Logos">
                     <div className="Logos">
-                        <SocialIcon url="https://twitter.com/shatterproofhq?lang=en" style={{margin: 8}}/>
-                        <SocialIcon url="https://www.facebook.com/ShatterproofHQ/?ref=nf" style={{margin: 8}}/>
-                        <SocialIcon url="https://www.instagram.com/weareshatterproof/?hl=en" style={{margin: 8}}/>
+                        <SocialIcon url="https://twitter.com/shatterproofhq?lang=en" target="_blank" style={{margin: 8}}/>
+                        <SocialIcon url="https://www.facebook.com/ShatterproofHQ/?ref=nf" target="_blank" style={{margin: 8}}/>
+                        <SocialIcon url="https://www.instagram.com/weareshatterproof/?hl=en" target="_blank" style={{margin: 8}}/>
                     </div>
                 </footer>
             </div>
